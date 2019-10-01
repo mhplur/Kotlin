9eb939be283f58dd92675806eac6eea6d36720df
Milton Paredes <mhplur@hotmail.com>
2019-10-01 15:54:44 -0500

    Change code into functions

--- Example.kt
+++ Example.kt
@@ -1,33 +1,36 @@
 fun main(args: Array<String>) {
-    // AREA AND PERIMETER OF A SQUARE
-    val side = 50 // INMUTABLE VAR
-    val area = side * 2 
+    getAreaAndPerimeter(50);
+    getAverageWeight(floatArrayOf(89.4f, 67f, 87.45f));
+    getApprovedOrReproved(intArrayOf(8,7,6,9,10))
+    countNumberDigits(25);
+}
+
+fun getAreaAndPerimeter(side : Int){
+    val area = side * 2 //INMUTABLE VAR
     val perimeter = side * 4
     println("Area's square: $area")
     println("Perimeter's square: $perimeter")
-    
-    // AVERAGE'S WEIGHTS
-    val weight1 = 89.4f
-    val weight2 = 67f
-    val weight3 = 87.45f
-    val average = (weight1 + weight2 + weight3) / 3
+}
+
+fun getAverageWeight(arrayWeights : FloatArray){
+    arrayWeights.sum()
+    val average = (arrayWeights.sum()) / 3
     println("The average of three weights is $average")
-    
-    // APPROVED OR REPROVED
-    var score1 = 2
-    var score2 = 3
-    var score3 = 9
-    var average = (score1 + score2 + score3) / 3
+}
+
+fun getApprovedOrReproved(arrayScores : IntArray){
+    val average = (arrayScores.sum()) / 3
+    print("The student is ")
     if(average > 7)
     	println("Approved")
     else
     	println("Reproved")
+}
 
-    // DIGIT COUNTER
-    val number = 54
+fun countNumberDigits(number : Int) : String{
     val message = "Number has"
-    if(number <10)
-    	println("$message 1 digit")
+    return if(number <10)
+    	"$message 1 digit"
     else
-    	println("$message more than 1 digit")
+    	"$message more than 1 digit"
 }
