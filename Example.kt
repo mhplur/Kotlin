0a6ccb058399197b9ad99c0ac61d8aa274b4d8a9
Milton Paredes <mhplur@hotmail.com>
2019-10-01 14:32:19 -0500

    Add Area and Perimeter
    Add Area and Perimeter of a square

--- Example.kt
+++ Example.kt
@@ -1,4 +1,12 @@
 fun main(args: Array<String>) {
+    // Area and Perimeter of a square
+    val side = 50 // inmutable var
+    val area = side * 2 
+	val perimeter = side * 4
+    println("Area's square: $area")
+    println("Perimeter's square: $perimeter")
+    
+    //Average's weights
     val weight1 = 89.4f
     val weight2 = 67f
     val weight3 = 87.45f
