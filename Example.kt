35fe40015a382117fc97deadbbeb016e73508f14
Milton Paredes <mhplur@hotmail.com>
2019-10-01 14:52:35 -0500

    Approved and Reproved

--- Example.kt
+++ Example.kt
@@ -6,10 +6,20 @@ fun main(args: Array<String>) {
     println("Area's square: $area")
     println("Perimeter's square: $perimeter")
     
-    //AVERAGE'S WEIGHTS
+    // AVERAGE'S WEIGHTS
     val weight1 = 89.4f
     val weight2 = 67f
     val weight3 = 87.45f
     val average = (weight1 + weight2 + weight3) / 3
     println("The average of three weights is $average")
+    
+    // APPROVED OR REPROVED
+    var score1 = 2
+    var score2 = 3
+	var score3 = 9
+    var average = (score1 + score2 + score3) / 3
+    if(average > 7)
+    	println("Approved")
+	else
+    	println("Reproved")
 }
