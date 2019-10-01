fcac23414a253fc49bcca1b19d49d54ce20116e6
Milton Paredes <mhplur@hotmail.com>
2019-10-01 14:22:15 -0500

    Create average
    Average of three weights

---
+++ Example.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val weight1 = 89.4f
+    val weight2 = 67f
+    val weight3 = 87.45f
+    val average = (weight1 + weight2 + weight3) / 3
+    println("The average of three weights is $average")
+}
