4ad723a9cb30b33fe96f283c9ff34734dc8c0567
Milton Paredes <mhplur@hotmail.com>
2019-10-01 14:33:56 -0500

    Change comments

--- Example.kt
+++ Example.kt
@@ -1,12 +1,12 @@
 fun main(args: Array<String>) {
-    // Area and Perimeter of a square
-    val side = 50 // inmutable var
+    // AREA AND PERIMETER OF A SQUARE
+    val side = 50 // INMUTABLE VAR
     val area = side * 2 
-	val perimeter = side * 4
+    val perimeter = side * 4
     println("Area's square: $area")
     println("Perimeter's square: $perimeter")
     
-    //Average's weights
+    //AVERAGE'S WEIGHTS
     val weight1 = 89.4f
     val weight2 = 67f
     val weight3 = 87.45f
